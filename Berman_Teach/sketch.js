dc411be27ff044da991beb49319c26b6e6045524
Sara Berman <37158727+saramberman@users.noreply.github.com>
2020-04-21 16:25:01 -0400

    Update sketch.js

--- Berman_Teach/sketch.js
+++ Berman_Teach/sketch.js
@@ -30,7 +30,7 @@ function setup() {
   color(100);
 //   createCanvas(1000, 600);
   // Create the video
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(900px, 750px);
   video = createCapture(VIDEO);
   video.hide();
   frameRate(fr);
